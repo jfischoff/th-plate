85eac462f61bcb17d5e2a3c6d126fae5f7fe4c85
Jonathan Fischoff <jonathan.fischoff@booyah.com>
2012-08-11 17:32:14 -0700

    got childTypes to compile, maybe it works?

--- src/Language/Haskell/TH/Plate/Folds.hs
+++ src/Language/Haskell/TH/Plate/Folds.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TupleSections, RankNTypes #-}
+{-# LANGUAGE TupleSections, RankNTypes, FlexibleInstances, DeriveFunctor #-}
 module Language.Haskell.TH.Plate.Folds where
 import Language.Haskell.TH.Plate.Types
 import Data.Generics.Multiplate
@@ -8,16 +8,29 @@ import Data.Traversable
 import Data.Functor.Constant
 import Data.Functor.Identity
 import Data.List
+import Data.Monoid
+import Control.Monad
 
-childTypes :: Name -> Q [Type]
+newtype MQ a = MQ { unMQ :: a}
+    deriving (Functor)
+    
+instance (Monad m, Monoid a) => Monoid (MQ (m a)) where
+    mempty  = MQ $ return mempty
+    mappend (MQ x) (MQ y) = MQ $ liftM2 mappend x y
+
+childTypes :: Info -> MQ (Q [Type])
 childTypes n = do 
-      let go (ConT n	       ) = Constant <$> childTypes n
-          go (VarT n	       ) = return $ Constant [] 
-          go (ArrowT	       ) = return $ Constant []
-          go (ListT	           ) = return $ Constant []
-          go x                   = return $ Constant [x]   
+      let go (ConT x ) = Constant $ MQ $ (unMQ . childTypes) =<< reify x
+          go (VarT _ ) = Constant $ MQ $ return [] 
+          go (ArrowT ) = Constant $ MQ $ return []
+          go (ListT	 ) = Constant $ MQ $ return []
+          go x         = Constant $ MQ $ return [x]   
           
-      return $ 
-          fmap nub $ foldFor infoPL (preorderFold purePlate { typPL = go } ) n
+ 
+      fmap nub <$> foldFor infoPL (preorderFold purePlate { typPL = go } ) n
 
-
+--Perhaps an alternate way...or correct have tested the code yet.
+--      roconnor: jfischoff: \proj plate -> foldFor proj (applyNaturalTransform eta plate)  where eta (Compose x) = Constant (getConstant `liftM` x)
+--      [5:14pm] roconnor: jfischoff: I think that has the type you requested
+--
+--
