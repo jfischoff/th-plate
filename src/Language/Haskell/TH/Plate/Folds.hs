37e00f22406204677151c091744a59eac9930061
Jonathan Fischoff <jonathan.fischoff@booyah.com>
2012-08-12 17:03:48 -0700

    added biplate version of childTypes

--- src/Language/Haskell/TH/Plate/Folds.hs
+++ src/Language/Haskell/TH/Plate/Folds.hs
@@ -2,14 +2,16 @@
 module Language.Haskell.TH.Plate.Folds where
 import Language.Haskell.TH.Plate.Types
 import Data.Generics.Multiplate
-import Language.Haskell.TH.Syntax
+import Language.Haskell.TH.Syntax hiding (lift)
 import Control.Applicative
-import Data.Traversable
+import Data.Traversable hiding (sequence)
 import Data.Functor.Constant
 import Data.Functor.Identity
 import Data.List
 import Data.Monoid
 import Control.Monad
+import Data.Generics.Uniplate.Data
+import Control.Monad.Reader
 
 newtype MQ a = MQ { unMQ :: a}
     deriving (Functor)
@@ -17,19 +19,37 @@ newtype MQ a = MQ { unMQ :: a}
 instance (Monad m, Monoid a) => Monoid (MQ (m a)) where
     mempty                = MQ $ return mempty
     mappend (MQ x) (MQ y) = MQ $ liftM2 mappend x y
+    
+type Context = ReaderT [Name] Q
 
-childTypes :: Info -> Q [Type]
+childTypes :: Info -> Context [Type]
 childTypes = 
      fmap nub . unMQ . foldFor infoPL (preorderFold purePlate { typPL = go } )  
         where
-          go x@(ConT n ) = Constant $ MQ $ fmap (x:) $ childTypes =<< reify n
-          go (VarT _ )   = Constant $ MQ $ return [] 
-          go (ArrowT )   = Constant $ MQ $ return []
-          go (ListT  )   = Constant $ MQ $ return []
-          go x           = Constant $ MQ $ return [x]   
+          go x@(ConT _ ) = Constant . MQ $ recurseOnce childTypes x
+          go (VarT _ )   = Constant . MQ $ return [] 
+          go (ArrowT )   = Constant . MQ $ return []
+          go (ListT  )   = Constant . MQ $ return []
+          go x           = Constant . MQ $ return [x] 
+
+--Using Biplate          
+childTypes' :: Info -> Context [Type] 
+childTypes' i = 
+    fmap (nub . concat) $ sequence $ [ go t | t <- universeBi i ] 
+        where
+          go x@(ConT _ ) = recurseOnce childTypes' x
+          go (VarT _ )   = return [] 
+          go (ArrowT )   = return []
+          go (ListT  )   = return []
+          go x           = return [x] 
           
  
-      
+recurseOnce :: (Info -> Context [Type]) -> Type -> Context [Type]
+recurseOnce f x@(ConT n ) = do 
+    has <- asks (elem n)
+    if has 
+        then return []
+        else local (n:) $ fmap (x:) $ f =<< (lift $ reify n)
 
 --Perhaps an alternate way...or correct have tested the code yet.
 --      roconnor: jfischoff: \proj plate -> foldFor proj (applyNaturalTransform eta plate)  where eta (Compose x) = Constant (getConstant `liftM` x)
